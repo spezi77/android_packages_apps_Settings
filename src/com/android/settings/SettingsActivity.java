0bf7ab6084c887b8ae6ac4a5f866c934047fb363
scott <scott.hart.bti@gmail.com>
2016-04-26 23:25:48 -0400

    Revert "Add Weather Content Provider [2/5]"
    This reverts commit ecf19b5331d5677292603987dd8ea453af7e1795.
    
    Signed-off-by: scott <scott.hart.bti@gmail.com>
    
    Conflicts:
    	AndroidManifest.xml
    	res/values/cm_strings.xml
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java
    
    Change-Id: Ie4dbf9dc81ffde1305c4ea36591888a6f0e5eef3

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -88,7 +88,6 @@ import com.android.settings.contributors.ContributorsCloudFragment;
 import com.android.settings.cyanogenmod.DisplayRotation;
 import com.android.settings.cyanogenmod.WakelockBlocker; 
 import com.android.settings.cyanogenmod.LiveLockScreenSettings;
-import com.android.settings.cyanogenmod.WeatherServiceSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -390,7 +389,6 @@ public class SettingsActivity extends Activity
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName(),
             LiveLockScreenSettings.class.getName(),
-            WeatherServiceSettings.class.getName(),
             HeadsUpSettings.class.getName()
     };
 
@@ -1350,11 +1348,6 @@ public class SettingsActivity extends Activity
                     boolean hasDeviceKeys = getResources().getInteger(
                             com.android.internal.R.integer.config_deviceHardwareKeys) != 0;
                     if (!hasDeviceKeys) {
-                        removeTile = true;
-                    }
-                } else if (id == R.id.weather_settings) {
-                    if (!getPackageManager().hasSystemFeature(
-                            CMContextConstants.Features.WEATHER_SERVICES)) {
                         removeTile = true;
                     }
                 }
