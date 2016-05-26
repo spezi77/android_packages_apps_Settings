6a7a8719f94666bcf47447b1fdeb3482fdffc779
scott <scott.hart.bti@gmail.com>
2016-05-25 23:34:40 -0400

    fix compile
    Change-Id: Iee9677d36fe26f0dbafb037813b3fd1c8467fdb2
    Signed-off-by: scott <scott.hart.bti@gmail.com>

--- src/com/android/settings/cyanogenmod/IconPickerGallery.java
+++ src/com/android/settings/cyanogenmod/IconPickerGallery.java
@@ -30,6 +30,8 @@ import android.os.Environment;
 import android.os.UserHandle;
 import android.widget.Toast;
 
+import com.android.settings.R;
+
 /**
  * So we can capture image selection in DUSystemReceiver
  */
