76605279ce7ec28c8fc91873da88229345188fff
pranav <cherukupallip@gmail.com>
2016-06-29 20:06:10 +0530

    [AE] Configurable Header Text Shadow (2/2)
    PS2: Add option to change shadow color
    PS3: Fix some tabs
    PS4: Change max value to 25 since that is the maximum allowed by the system
    PS5: When You Change Online But Don't Cherry-Pick Locally -_-
    
    Change-Id: I26d209763ed49a635ac4bcbd450277a0688a8cda
    Signed-off-by: pranav <cherukupallip@gmail.com>
    Signed-off-by: scott <scott.hart.bti@gmail.com>

--- src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
@@ -45,6 +45,8 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment  impl
     private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
     private static final String PREF_STATUS_BAR_HEADER_FONT_STYLE = "status_bar_header_font_style";
     private static final String CUSTOM_HEADER_IMAGE_SHADOW = "status_bar_custom_header_shadow";
+    private static final String CUSTOM_HEADER_TEXT_SHADOW = "status_bar_custom_header_text_shadow";
+    private static final String CUSTOM_HEADER_TEXT_SHADOW_COLOR = "status_bar_custom_header_text_shadow_color";
     private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
     private static final String PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
     private static final String PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
@@ -57,9 +59,13 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment  impl
     private ListPreference mNumColumns;
     private ListPreference mNumRows;
     private SeekBarPreferenceCham mHeaderShadow;
+    private SeekBarPreferenceCham mTextShadow;
+    private ColorPickerPreference mTShadowColor;
     private ListPreference mTileAnimationStyle;
     private ListPreference mTileAnimationDuration;
     private ListPreference mTileAnimationInterpolator;
+
+    static final int DEFAULT_HEADER_SHADOW_COLOR = 0xFF000000;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -98,6 +104,23 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment  impl
         mNumRows.setValue(String.valueOf(numRows));
         updateNumRowsSummary(numRows);
         mNumRows.setOnPreferenceChangeListener(this);
+
+	// Status Bar header text shadow
+        mTextShadow = (SeekBarPreferenceCham) findPreference(CUSTOM_HEADER_TEXT_SHADOW);
+        final float textShadow = Settings.System.getFloat(resolver,
+                Settings.System.STATUS_BAR_CUSTOM_HEADER_TEXT_SHADOW, 0);
+        mTextShadow.setValue((int)(textShadow));
+        mTextShadow.setOnPreferenceChangeListener(this);
+
+        //Status Bar header text shadow color
+        mTShadowColor =
+                (ColorPickerPreference) findPreference(CUSTOM_HEADER_TEXT_SHADOW_COLOR);
+        mTShadowColor.setOnPreferenceChangeListener(this);
+        int shadowColor = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_CUSTOM_HEADER_TEXT_SHADOW_COLOR, DEFAULT_HEADER_SHADOW_COLOR);
+        String HexColor = String.format("#%08x", (0x000000 & shadowColor));
+        mTShadowColor.setSummary(HexColor);
+        mTShadowColor.setNewPreviewColor(shadowColor);
 
 	mHeaderShadow = (SeekBarPreferenceCham) findPreference(CUSTOM_HEADER_IMAGE_SHADOW);
         final int headerShadow = Settings.System.getInt(resolver,
@@ -217,6 +240,20 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment  impl
             Settings.System.putInt(getContentResolver(),
                     Settings.System.STATUS_BAR_CUSTOM_HEADER,
                     (Boolean) newValue ? 1 : 0);
+            return true;
+	} else if (preference == mTextShadow) {
+            float textShadow = (Integer) newValue;
+            float realHeaderValue = (float) ((double) textShadow);
+            Settings.System.putFloat(resolver,
+                  Settings.System.STATUS_BAR_CUSTOM_HEADER_TEXT_SHADOW, realHeaderValue);
+             return true;
+        } else if (preference == mTShadowColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(resolver,
+                    Settings.System.STATUS_BAR_CUSTOM_HEADER_TEXT_SHADOW_COLOR, intHex);
             return true;
 	} else if (preference == mHeaderShadow) {
            Integer headerShadow = (Integer) newValue;
